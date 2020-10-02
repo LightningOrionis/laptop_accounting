c0adad0c44c02fd7e30d3f17f368986a3b31969b
LightningOrionis <krasovsky17igor@gmail.com>
2020-10-03 00:15:10 +0300

    adding 1 test class with issue

--- core/tests.py
+++ core/tests.py
@@ -1,5 +1,64 @@
-from django.test import TestCase
+from datetime import datetime
 
+from django.test import TestCase, Client
+
+from .models import *
 
 class HttpResponseTest(TestCase):
-    pass
+
+    def setUp(self):
+        self.c = Client()
+        w = Worker.objects.create(name='test_worker', team='testers')
+        i = Item.objects.create(title='item', type='NB', price=200,
+                                configuration_link='https://hello.world')
+        borrowed_item = BorrowedItem.objects.create(item=i, borrower=w,
+                                                    paid_by='abc', comment='aa')
+
+
+    def test_connection_index(self):
+        response = self.c.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_borrowed_list(self):
+        response = self.c.get('/borrowed_list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_workers_list(self):
+        response = self.c.get('/workers_list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_items_list(self):
+        response = self.c.get('/items_list')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_create_worker(self):
+        response = self.c.get('/new_worker')
+        self.assertEqual(response.status_code, 200)
+
+    def test_coonection_update_worker(self):
+        response = self.c.get('/worker/1/update')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_delete_worker(self):
+        response = self.c.get('/worker/1/delete')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_worker_detail_8(self):
+        response = self.c.get('/worker/1')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_create_item(self):
+        response = self.c.get('/new_item')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_update_item(self):
+        response = self.c.get('/item/1/update')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_delete_item(self):
+        response = self.c.get('/item/1/delete')
+        self.assertEqual(response.status_code, 200)
+
+    def test_connection_borrow_item(self):
+        response = self.c.get('/item/1/borrow')
+        self.assertEqual(response.status_code, 200)
